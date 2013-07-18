a7d1da993709da7c354d97e9ba511b476e1cbe4b
Oleg Kostyuk <cub.uanic@gmail.com>
2013-07-18 23:35:26 +0300

    Implemented parametrized types

--- lib/Validator/Declarative/Rules/ParametrizedType.pm
+++ lib/Validator/Declarative/Rules/ParametrizedType.pm
@@ -7,27 +7,115 @@ package Validator::Declarative::Rules::ParametrizedType;
 # ABSTRACT: Declarative parameters validation - default simple types rules
 
 use Error qw/ :try /;
+use Scalar::Util qw/ blessed looks_like_number /;
 require Validator::Declarative;
 
 #
 # INTERNALS
 #
 
+sub _validate_min {
+    my ( $input, $param ) = @_;
+    no warnings;
+    throw Error::Simple('does not satisfy MIN')
+        if !looks_like_number($input) || !looks_like_number($param) || $input < $param;
+}
+
+sub _validate_max {
+    my ( $input, $param ) = @_;
+    no warnings;
+    throw Error::Simple('does not satisfy MAX')
+        if !looks_like_number($input) || !looks_like_number($param) || $input > $param;
+}
+
+sub _validate_ref {
+    my ( $input, $param ) = @_;
+    throw Error::Simple('does not satisfy REF') if !ref($input);
+    return if !$param;
+    $param = [$param] if ref($param) ne 'ARRAY';
+    ref($input) eq $_ && return for @$param;
+    throw Error::Simple('does not satisfy REF');
+}
+
+sub _validate_class {
+    my ( $input, $param ) = @_;
+    throw Error::Simple('does not satisfy CLASS') if !blessed($input);
+    return if !$param;
+    $param = [$param] if ref($param) ne 'ARRAY';
+    $input->isa($_) && return for @$param;
+    throw Error::Simple('does not satisfy CLASS');
+}
+
+sub _validate_can {
+    my ( $input, $param ) = @_;
+    throw Error::Simple('does not satisfy CAN') if !blessed($input) || !$param;
+    $param = [$param] if ref($param) ne 'ARRAY';
+    $input->can($_) || throw Error::Simple('does not satisfy CAN') for @$param;
+    return;
+}
+
+sub _validate_can_any {
+    my ( $input, $param ) = @_;
+    throw Error::Simple('does not satisfy CAN_ANY') if !blessed($input) || !$param;
+    $param = [$param] if ref($param) ne 'ARRAY';
+    $input->can($_) && return for @$param;
+    throw Error::Simple('does not satisfy CAN_ANY');
+}
+
+sub _validate_any_of {
+    my ( $input, $param ) = @_;
+    throw Error::Simple('does not satisfy ANY_OF') if !$param;
+    $param = [$param] if ref($param) ne 'ARRAY';
+    _smart_match( $input, $_ ) && return for @$param;
+    throw Error::Simple('does not satisfy ANY_OF');
+}
+
+sub _validate_list_of {
+    my ( $input, $param ) = @_;
+    ## TBD in next release
+}
+
+sub _validate_hash_of {
+    my ( $input, $param ) = @_;
+    ## TBD in next release
+}
+
+sub _validate_hash {
+    my ( $input, $param ) = @_;
+    ## TBD in next release
+}
+
+sub _validate_date {
+    my ( $input, $param ) = @_;
+    ## TBD in next release
+}
+
+# this is limited subset of smart-match operator (~~) from Perl 5.10+
+sub _smart_match {
+    my ( $x, $y ) = @_;
+    looks_like_number($x) && looks_like_number($y) && return $x == $y;
+    ref($y) eq 'CODE' && return $y->($x);
+    ref($y) eq 'Regexp' && return $x =~ m/$y/;
+    return $x eq $y;
+}
+
 sub _register_default_parametrized_types {
     Validator::Declarative::register_type(
         ## simple parametrized types
-        min    => \&Validator::Declarative::_validate_pass,    # \&validate_min,
-        max    => \&Validator::Declarative::_validate_pass,    # \&validate_max,
-        ref    => \&Validator::Declarative::_validate_pass,    # \&validate_ref,
-        class  => \&Validator::Declarative::_validate_pass,    # \&validate_class,
-        enum   => \&Validator::Declarative::_validate_pass,    # \&validate_any_of,
-        any_of => \&Validator::Declarative::_validate_pass,    # \&validate_any_of,
-
+        min       => \&_validate_min,
+        max       => \&_validate_max,
+        ref       => \&_validate_ref,
+        class     => \&_validate_class,
+        duck_type => \&_validate_can,
+        can       => \&_validate_can,
+        can_any   => \&_validate_can_any,
+        enum      => \&_validate_any_of,
+        any_of    => \&_validate_any_of,
         ## complex/recursive parametrized types
-        list_of => \&Validator::Declarative::_validate_pass,    # undef,
-        hash_of => \&Validator::Declarative::_validate_pass,    # undef,
-        hash    => \&Validator::Declarative::_validate_pass,    # undef,
-        date    => \&Validator::Declarative::_validate_pass,    # undef,
+        list_of => \&Validator::Declarative::_validate_pass,
+        hash_of => \&Validator::Declarative::_validate_pass,
+        hash    => \&Validator::Declarative::_validate_pass,
+        date    => \&Validator::Declarative::_validate_pass,
     );
 }
 
--- t/lib/Test/Validator/Declarative.pm
+++ t/lib/Test/Validator/Declarative.pm
@@ -8,6 +8,7 @@ package Test::Validator::Declarative;
 use Exporter;
 use Test::More;
 use Test::Exception;
+use Data::Dumper;
 use Validator::Declarative;
 
 our @ISA       = qw/ Exporter /;
@@ -15,45 +16,72 @@ our @EXPORT_OK = qw/ check_type_validation /;
 
 sub check_type_validation {
     my %param = @_;
-    plan tests => 4    # for lives_ok + is_deeply
-        + 1 + scalar( @{ $param{bad} } );    # throws_ok + message for each error
-    check_type_validation_pass( $param{type}, $param{good} );
-    check_type_validation_fail( $param{type}, $param{bad} );
-}
 
-sub check_type_validation_pass {
-    my ( $type, $values ) = @_;
-    my @result;
+    # for lives_ok + is_deeply + throws_ok + message for each error
+    plan tests => 4 + 1 + scalar( @{ $param{bad} } );
 
+    my ( $type, $values, @result, $type_name, $stringified_type );
+
+    $type = $param{type};
+
+    ($type_name) =    # there should be exactly one k/v pair
+          ref($type) eq 'HASH'  ? keys(%$type)
+        : ref($type) eq 'ARRAY' ? $type->[0]
+        :                         $type;
+
+    $stringified_type = struct_to_str($type);
+
+    #
+    # check type validation pass
+    #
+    $values = $param{good};
     lives_ok {
-        @result = Validator::Declarative::validate( [undef] => [ "param_${type}_0" => [ 'optional', $type ] ] );
+        @result = Validator::Declarative::validate( [undef] => [ "param_${type_name}_0" => [ 'optional', $type ] ] );
     }
-    "type 'optional,$type' lives on undef";
-    is_deeply( \@result, [undef], "type 'optional,$type' returns expected result" );
+    "type 'optional,$stringified_type' lives on undef";
+    is_deeply( \@result, [undef], "type 'optional,$stringified_type' returns expected result" );
 
     lives_ok {
         @result = Validator::Declarative::validate(
-            $values => [ map { sprintf( "param_${type}_%02d", $_ ) => $type, } 1 .. scalar @$values ] );
+            $values => [ map { sprintf( "param_${type_name}_%02d", $_ ) => $type, } 1 .. scalar @$values ] );
     }
-    "type $type lives on correct parameters";
-    is_deeply( \@result, $values, "type $type returns expected result" );
-}
+    "type $stringified_type lives on correct parameters";
+    is_deeply( \@result, $values, "type $stringified_type returns expected result" );
 
-sub check_type_validation_fail {
-    my ( $type, $values ) = @_;
-
+    #
+    # check type validation fail
+    #
+    $values = $param{bad};
     throws_ok {
         Validator::Declarative::validate(
-            $values => [ map { sprintf( "param_${type}_%02d", $_ ) => $type, } 1 .. scalar @$values ] );
+            $values => [ map { sprintf( "param_${type_name}_%02d", $_ ) => $type, } 1 .. scalar @$values ] );
     }
-    'Error::Simple', "type $type throws on incorrect parameters";
+    'Error::Simple', "type $stringified_type throws on incorrect parameters";
 
     my $error_text = "$@";
     for ( 1 .. scalar @$values ) {
-        my $param = sprintf( "param_${type}_%02d", $_ );
-        my $regexp = sprintf( "%s: .* does not satisfy %s", $param, uc($type) );
+        my $param = sprintf( "param_${type_name}_%02d", $_ );
+        my $regexp = sprintf( "%s: .* does not satisfy %s", $param, uc($type_name) );
         like $error_text, qr/^$regexp/m, "message about $param";
     }
+
+}
+
+sub struct_to_str {
+    my ( $struct, $maxdepth, $use_deparse ) = @_;
+
+    $maxdepth    ||= 3;
+    $use_deparse ||= 0;
+
+    local $Data::Dumper::Deparse   = $use_deparse;
+    local $Data::Dumper::Indent    = 0;
+    local $Data::Dumper::Maxdepth  = $maxdepth;
+    local $Data::Dumper::Quotekeys = 1;
+    local $Data::Dumper::Sortkeys  = 1;
+    local $Data::Dumper::Terse     = 1;
+    local $Data::Dumper::Useqq     = 0;
+
+    return Data::Dumper::Dumper($struct);
 }
 
 1;
